2323e68bce8ea0a5078922a1bd8d54fe6123c435
fehergg <feherg.24d@acsjszki.hu>
2025-02-12 14:22:12 +0100

    js

---
+++ script.js
@@ -0,0 +1,40 @@
+
+// Kiírás a konzolra:
+console.log("Hello world!");
+
+//Egysoros Komment
+
+/*
+
+fdf
+dfd
+fdfdfdf
+többsoros
+ komment*/
+
+ //Adattípusok
+
+ //1. szöveg (string)
+ console.log("ez egy szoveg");
+
+ //2. szám (number)
+ console.log(123);
+
+ //3. logikai típus (bool)
+ //két értéket vehet fel; true/false
+ console.log(true);
+ console.log(false);
+
+ //4. undefined
+ console.log(undefined);
+
+ //5. null
+ console.log(null)
+
+ //összetett adattípusok
+
+ //1. tömb(array) []
+ console.log(["Gipsz","Jakab",18]);
+
+ //2. Objektum (Object) {}
+ console.log({"firstname": "Jakab"})
